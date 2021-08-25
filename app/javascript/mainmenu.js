3c9daf6d653d75b067ded9330a0514b41d54f3b2
mkato1013 <mkatochoco1013@gmail.com>
2021-08-25 13:51:38 +0900

    投稿ページデザイン編集

---
+++ app/javascript/mainmenu.js
@@ -0,0 +1,23 @@
+function pullDownSearchForm() {
+
+  const pullDownButton = document.getElementById("back-btn");
+  const searchForm = document.getElementById("pull-down");
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "color:red;");
+  });
+
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "color:red;");
+  });
+
+  pullDownButton.addEventListener('click', function() {
+    if (searchForm.getAttribute("style") == "display:block;"){
+      searchForm.removeAttribute("style", "display:block;")
+    } else {
+      searchForm.setAttribute("style", "display:block;")
+    };
+  });
+};
+
+window.addEventListener('load' , pullDownSearchForm);

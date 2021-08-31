0e4a0428a671d6df93851b0ab6a02e29a80fed80
mkato1013 <mkatochoco1013@gmail.com>
2021-08-31 15:11:06 +0900

    マイページ編集ページ遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,30 @@ class UsersController < ApplicationController
     @users = user.followers
   end
 
-  def index
-    @users = User.where.not(id: current_user.id)
+  # def index
+  #   @users = User.where.not(id: current_user.id)
+  # end
+
+  def edit
+    @user = User.find(params[:id])
+    return redirect_to user_path if current_user.id != @user.id
+  end
+
+  def update
+    @user = User.find(params[:id])
+    return redirect_to user_path if current_user.id != @user
+
+    if @user.update(user_params)
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:icon, :nickname, :email, :introduction)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :following
 
   
-  has_one_attached :image
+  has_one_attached :icon
   
   def already_liked?(food)
     self.likes.exists?(food_id: food)

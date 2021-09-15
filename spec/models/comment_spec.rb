bf9b18df95e090c353e84aa699c021c8b2a4d29c
mkato1013 <mkatochoco1013@gmail.com>
2021-09-15 18:16:41 +0900

    コメント機能テストコード記入

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   before do
-    @comment = FactoryBot.build(:comment)
+    user = FactoryBot.create(:user)
+    food = FactoryBot.create(:food)
+    @comment = FactoryBot.build(:comment, user_id: user.id, food_id: food.id)
+    sleep 0.2
   end
 
   describe 'コメント機能' do
@@ -10,15 +13,25 @@ RSpec.describe Comment, type: :model do
       it "コメント文を入力済みであれば保存できる" do
         expect(@comment).to be_valid
       end
+    end
 
     context 'コメントを保存できない場合' do
       it "コメントが空では投稿できない" do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include "Text can't be blank"
       end
 
       it "ユーザーがログインしていなければコメントできない" do
+        @comment.user_id = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include "User must exist"
       end
 
       it "投稿したものがなければコメントできない" do
+        @comment.food_id = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include "Food must exist"
       end
 
     end
